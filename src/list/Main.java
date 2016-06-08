fa997813d17856735e8f09d8ca5b840863e6938e
potaychuk <sviatoslavpotaychuk@gmail.com>
2016-06-08 16:44:42 +0300

    1

--- src/list/Main.java
+++ src/list/Main.java
@@ -1,28 +1,51 @@
 package list;
 
+
+
+import java.util.Iterator;
+
 public class Main {
+    public Main() {
+    }
 
     public static void main(String[] args) {
-        SimpleArrayList<String> list = new SimpleArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add("5");
-        list.add("6");
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add("5");
-        list.add("6");
+        MyTreeSet tree = new MyTreeSet();
+        tree.add(Integer.valueOf(7));
+        tree.add(Integer.valueOf(2));
+        tree.add(Integer.valueOf(11));
+        tree.add(Integer.valueOf(1));
+        tree.add(Integer.valueOf(6));
+        tree.add(Integer.valueOf(9));
+        tree.add(Integer.valueOf(14));
+        tree.add(Integer.valueOf(4));
+        tree.add(Integer.valueOf(10));
+        tree.add(Integer.valueOf(12));
+        tree.add(Integer.valueOf(3));
+        tree.add(Integer.valueOf(5));
+        Iterator i$ = tree.iterator();
 
-        for (String s : list){
-            System.out.println(s);
+        Integer node;
+        while(i$.hasNext()) {
+            node = (Integer)i$.next();
+            System.out.println(node);
         }
 
+        tree.remove(Integer.valueOf(11));
+        System.out.println("New");
+        i$ = tree.iterator();
 
+        while(i$.hasNext()) {
+            node = (Integer)i$.next();
+            System.out.println(node);
+        }
 
+        tree.remove(Integer.valueOf(12));
+        i$ = tree.iterator();
+
+        while(i$.hasNext()) {
+            node = (Integer)i$.next();
+            System.out.println(node);
+        }
 
     }
 }
